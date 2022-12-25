8dc680a723a3a5eca911c82f783f9269318c8bff
Mitchell, Elijah C <elicmitc@iu.edu>
2022-12-25 16:25:19 -0500

    Add supporting lab1 lab2 and readme files

---
+++ LAB2/bitops.c
@@ -0,0 +1,83 @@
+#include "bitops.h"
+
+/*
+ * Return and of x and y
+ * Points: 3
+ */
+int and_op(int x, int y) {
+  return 2;
+}
+
+/* 
+ * Return or of x and y
+ * Points: 3
+ */
+int or_op(int x, int y) {
+  return 2;
+}
+
+/*
+ * Return xor of x and y
+ * Points: 3
+ */
+int xor_op(int x, int y) {
+  return 2;
+}
+
+
+/*
+ * Clear `b`th byte of x
+ * Points: 7
+ */
+int masking_and(int x, int b) {
+  return 2;
+}
+
+/*
+ * Set `b`th byte of x
+ * Points: 7
+ */
+int masking_or(int x, int b) {
+  return 2;
+}
+
+/*
+ * Toggle `b`th byte of x
+ * Points: 7
+ */
+int masking_xor(int x, int b) {
+  return 2;
+}
+
+/*
+ * Shift x to the left by `sc` # of bits
+ * Points: 3
+ */
+int shifting_left_op(int x, int sc) {
+  return 2;
+}
+
+/*
+ * Shift x to the right by `sc` # of bits
+ * Points: 3
+ */
+int shifting_right_op(int x, int sc) {
+  return 2;
+}
+
+
+/*
+ * Return logical not of x
+ * Points: 3
+ */
+int bang(int x) {
+  return 2;
+}
+
+/*
+ * Print binary string representation for x
+ * Points: 30
+ */
+void bit_conversion(int x) {
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Welcome to Spring 2022 Computer Structures/CSE Repo

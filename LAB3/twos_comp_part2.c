cacb58e1c65f27a6f90df2d6c80991990d14c00c
Carter Novak <novakc@iu.edu>
2023-02-02 13:24:10 -0500

    LAB3 Solutions

--- LAB3/twos_comp_part2.c
+++ LAB3/twos_comp_part2.c
@@ -6,8 +6,19 @@
 int main(int argc, char* argv[]){
 
 	if(argc != 3){
-		printf("Error: Incorrect number of arguments");
+		printf("Error: Incorrect number of arguments\n");
 	       	exit(EXIT_FAILURE);	
+	}
+
+	size_t length = strlen(argv[1]);
+	if(length != 8){
+		printf("Error: Binary number entered is not 8 bits\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if(strcmp(argv[2], "signed") != 0 && strcmp(argv[2], "unsigned") != 0){
+		printf("Error: Second argument is incorrect\n");
+		exit(EXIT_FAILURE);
 	}
 
 	char *bin = argv[1];
@@ -18,11 +29,11 @@ int main(int argc, char* argv[]){
 				result -= (int)pow(2, 7 - i);
 			else if(i > 0 && bin[i] == '1')
 				result += (int)pow(2, 7 - i);
-	}else
+	}else{
 		for(int i = 0; i < 8; i++)
 			if(bin[i] == '1')
-				result += (int)pow(2, 7 - i);;
-
+				result += (int)pow(2, 7 - i);
+	}
 	printf("%d\n", result);
 	exit(EXIT_SUCCESS);
 }

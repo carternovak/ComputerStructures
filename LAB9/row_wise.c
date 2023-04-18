05a1c9e48834135209f7feb90e98be29766f62b3
Carter Novak <novakc@iu.edu>
2023-04-18 18:43:59 -0400

    LAB9 Solutions

---
+++ LAB9/row_wise.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+#define SZ 50000
+
+float A[SZ][SZ];
+
+int main(){
+
+	double time_spent = 0.0;
+	clock_t begin = clock();
+
+    	for(int i = 0; i < SZ; i++){
+    		for(int j = 0; j < SZ; j++){
+    	        	A[i][j] = i * j;
+    		}
+    	}
+    
+    	for(int i = 0; i < SZ; i++){
+    		for(int j = 1; j < SZ; j++){
+    	        	A[i][j] = (A[i][j - 1] + A[i][j]) / 2;
+    		}
+    	}
+	clock_t end = clock();
+	time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("Time: %f micro seconds\n", time_spent * 1000000);
+   
+    	return 0;
+}
